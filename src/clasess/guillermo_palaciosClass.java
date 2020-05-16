9e3e85c507fc975658608ecd6635f9483fd647d4
palaciosky <63530913+palaciosky@users.noreply.github.com>
2020-05-16 17:55:18 -0600

    CLASE GUILLERMO PALACIOS

---
+++ src/clasess/guillermo_palaciosClass.java
@@ -0,0 +1,17 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package clasess;
+
+/**
+ *
+ * @author Palacios
+ */
+public class guillermo_palaciosClass {
+    guillermo_palaciosClass(){
+    
+        System.out.println("Soy Guillermo Palacios 192219");
+    }
+}
